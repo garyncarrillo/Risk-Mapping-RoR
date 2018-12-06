5d5567be370ae64789cc7b6f48b018dfe809787b
Garyn Carrillo Caballero <garync@gmail.com>
2018-12-06 15:46:23 -0500

    servicio de creacion de paquete
    se crea el api para la creacion de paquete validando si el procedimiento existe en el mapa de procesos

--- raudit/app/controllers/api/tipo_procedimientos_controller.rb
+++ raudit/app/controllers/api/tipo_procedimientos_controller.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Api::TipoProcedimientosController < ApplicationController
 
 	def get_Tipo 
@@ -33,21 +35,27 @@ class Api::TipoProcedimientosController < ApplicationController
         
         @procedimientos.each do |procedimiento|
         	@id_procedimieto = procedimiento.id
-        	puts procedimiento.id
-        	puts procedimiento.nombre
         end
         
        	@paquete = PaquetePlan.where(nombre:@name_paquete)
+
         @paquete.each do |paquetes|
         	@id_paquete = paquetes.id
-        	puts paquetes.id
-        	puts paquetes.nombre
         end
-
-        @create_paquete_to_procedimiento = PaquetePlanToProcedimiento.create(idpaquete:@id_paquete, idprocedimiento:@id_procedimieto)
-        print @create_paquete_to_procedimiento
+        if @id_paquete.blank? or @id_procedimieto.blank? then 
+        	puts 'No existe Id de paquete ni procedimiento '
+        	# para validar si es  null o no 
+        	# if @object.present? then 
+        	error = {"nombre":"909","error":"el procedimiento o paquete no existe en la base de datos"}
+        	@id_procedimieto = error
+        else
+           @create_paquete_to_procedimiento = PaquetePlanToProcedimiento.create(idpaquete:@id_paquete, idprocedimiento:@id_procedimieto)
+           puts @create_paquete_to_procedimiento
+           puts 'Se creo correctamente '
+        end 
+        
         
     	#http://localhost:3000/api/setpaquetes_x_procedimiento?nombre_paquete=Proyectos&nombre_proceso=vivienda&procedimiento=Procedimiento%20Administraci%C3%B3n%20de%20Versiones%20V2
-    	render json:{back_end: @procedimientos }, status: :ok  
+    	render json:{back_end: @id_procedimieto }, status: :ok  
     end
 end
